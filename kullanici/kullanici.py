095a41eed08bff88fa1e09e8699ca774572e929f
lercrel <119791701+lercrel@users.noreply.github.com>
2022-12-04 16:00:42 +0300

    Update kullanici.py

--- kullanici/kullanici.py
+++ kullanici/kullanici.py
@@ -1,8 +1,9 @@
 # input, tkinter veya doğrudan kodla alınan bilgiler ile Kullanici class'ında yeni obje
 # oluşturulup json'a kaydedilir ve çekildiğinde pandas dataframei olarak ekrana yazdırılır.
-
+"""
 from datetime import datetime
 import pandas as pd
+
 x = pd.DataFrame(
     {
         "Ad": ["John", "Jane"],
@@ -10,8 +11,27 @@ x = pd.DataFrame(
         "Yaş": [10,20],
         "Doğum tarihi": [2022, 2010]
      })
-class Kullanici:
-    def __init__(self, ad, soyad, yas, dogumtarihi):
-        print("Yeni kullanıcı başarıyla kaydedildi!")
+"""
+
+
+class User:
+    def __init__(self, ad):
+        self.ad = ad
+    def init(self, dogumtarihi, sifre, kadi=None, tad=None, soyad=None):
+        self.dogumtarihi = dogumtarihi
+        self.sifre = sifre
+        self.kadi = self.ad if kadi is None else kadi
+        self.tad = self.ad if tad is None else tad
+        self.soyad = self.ad if soyad is None else soyad
+        print(f"Created user {self.ad}")
+    def signin(self, sifre):
+        if sifre == self.sifre:
+            print(f"Authenticated for {self.ad}")
+        def createproject(name):
+            print("hey")
+            
+
+#ad = input
+#dogumtarihi = input()
 
 
